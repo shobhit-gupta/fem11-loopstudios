ab63d26d867e26c66d2c79078b70e6b30ed2e362
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-05-09 12:31:48 +0000

    style: Add global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				darkGray: 'hsl(0, 0%, 55%)',
+				veryDarkGray: 'hsl(0, 0%, 41%)'
+			},
+			fontFamily: {
+				sans: ['Alata', 'sans-serif'],
+				display: ['Josefin Sans', 'sans-serif']
+			}
+		}
+	},
+	plugins: []
+};

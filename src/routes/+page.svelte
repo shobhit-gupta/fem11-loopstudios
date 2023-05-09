0bc201622a75f65f881c9a9437002bc70d151ade
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-05-09 12:12:58 +0000

    init: Add assets and starter files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-screen items-center justify-center">
+	<h1 class="text-3xl">Loopstudios Landing page</h1>
+</div>
+
+<!-- About -->
+<!-- Careers -->
+<!-- Events -->
+<!-- Products -->
+<!-- Support -->
+
+<!-- Immersive experiences that deliver -->
+
+<!-- The leader in interactive VR -->
+
+<!-- Founded in 2011, Loopstudios has been producing world-class virtual reality  -->
+<!-- projects for some of the best companies around the globe. Our award-winning  -->
+<!-- creations have transformed businesses through digital experiences that bind  -->
+<!-- to their brand. -->
+
+<!-- Our creations -->
+
+<!-- See all -->
+
+<!-- Deep earth -->
+<!-- Night arcade -->
+<!-- Soccer team VR -->
+<!-- The grid -->
+<!-- From up above VR -->
+<!-- Pocket borealis -->
+<!-- The curiosity -->
+<!-- Make it fisheye -->
+
+<!-- About -->
+<!-- Careers -->
+<!-- Events -->
+<!-- Products -->
+<!-- Support -->
+
+<!-- © 2021 Loopstudios. All rights reserved. -->
